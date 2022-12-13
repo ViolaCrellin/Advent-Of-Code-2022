48587cea82723f3fba3b2da9e8a8a14b10e7d28c
Viola Crellin <Viola.Crellin@splashdamage.com>
2022-12-13 18:00:54 +0000

    part 2 13

--- solutions/thirteen.go
+++ solutions/thirteen.go
@@ -3,6 +3,7 @@ package solutions
 import (
 	"fmt"
 	"reflect"
+	"sort"
 	"strings"
 
 	"example.com/adventofcode/util"
@@ -12,14 +13,41 @@ import (
 func Thirteen(input string) string {
 	signalPairs := strings.Split(input, "\n\n")
 	areOrdered := make([]int, len(signalPairs))
+	allSignals := []interface{}{}
 	for i := range signalPairs {
 		pair := strings.Split(signalPairs[i], "\n")
 		left := util.ParseStringIntSliceRepresentationJSON(pair[0])
 		right := util.ParseStringIntSliceRepresentationJSON(pair[1])
-		//fmt.Printf("\n LEFT %+v", left)
-		//fmt.Printf("\n RIGHT %+v", right)
 		areOrdered[i] = IsOrdered(left, right)
-		fmt.Printf("\n ORDERED %d", areOrdered[i])
+		if areOrdered[i] == 1 {
+			allSignals = append(allSignals, left)
+			allSignals = append(allSignals, right)
+		} else {
+			allSignals = append(allSignals, right)
+			allSignals = append(allSignals, left)
+		}
+	}
+
+	extra2 := util.ParseStringIntSliceRepresentationJSON("[[2]]")
+	extra6 := util.ParseStringIntSliceRepresentationJSON("[[6]]")
+	allSignals = append(allSignals, extra2)
+	allSignals = append(allSignals, extra6)
+	sort.Slice(allSignals, func(i, j int) bool {
+		return IsOrdered(convert.SliceOfInterface(allSignals[i]), convert.SliceOfInterface(allSignals[j])) == 1
+	})
+
+	index2 := 0
+	index6 := 0
+	for i := range allSignals {
+		signal := allSignals[i]
+		signalStr := fmt.Sprintf("%+v", signal)
+		if signalStr == fmt.Sprintf("%+v", extra2) {
+			index2 = i + 1
+		}
+		if signalStr == fmt.Sprintf("%+v", extra6) {
+			index6 = i + 1
+		}
+		fmt.Println(signalStr)
 	}
 
 	indicesSum := 0
@@ -31,7 +59,8 @@ func Thirteen(input string) string {
 		}
 	}
 	fmt.Printf("\n\n ORDERED INDEXES: %+v", orderedIndexes)
-	return fmt.Sprintf("\nPart 1: %d", indicesSum)
+
+	return fmt.Sprintf("\nPart 1: %d, Part 2: %d", indicesSum, index2*index6)
 }
 
 func IsSlice(v interface{}) bool {
@@ -39,17 +68,11 @@ func IsSlice(v interface{}) bool {
 }
 
 func IsOrdered(left []interface{}, right []interface{}) int {
-	fmt.Printf("\n COMPARING LEFT %+v VS RIGHT %+v", left, right)
-	// if len(left) == 0 && len(right) > 0 {
-	// 	return 1
-	// }
-
 	for i, leftItem := range left {
 		if i > len(right)-1 {
 			return 0
 		}
 		rightItem := right[i]
-		fmt.Printf("\n COMPARING LEFT ITEM %+v VS RIGHT ITEM %+v", leftItem, rightItem)
 		if IsFloat64(leftItem) && IsFloat64(rightItem) {
 			if convert.Int(leftItem) < convert.Int(rightItem) {
 				return 1
@@ -71,23 +94,14 @@ func IsOrdered(left []interface{}, right []interface{}) int {
 			}
 
 			return IsOrdered(convert.SliceOfInterface(leftItem), convert.SliceOfInterface(rightItem))
-			// if isOrdered == 0 {
-			// 	return 0
-			// }
 		}
 
 		if IsSlice(leftItem) && IsFloat64(rightItem) {
 			return IsOrdered(convert.SliceOfInterface(leftItem), convert.SliceOfInterface([]interface{}{rightItem}))
-			// if isOrdered == 0 {
-			// 	return 0
-			// }
 		}
 
 		if IsFloat64(leftItem) && IsSlice(rightItem) {
 			return IsOrdered(convert.SliceOfInterface([]interface{}{leftItem}), convert.SliceOfInterface(rightItem))
-			// if isOrdered == 0 {
-			// 	return 0
-			// }
 		}
 	}
 
